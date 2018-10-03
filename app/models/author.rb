9286e0f193fb5885a6b9a789ac8ed4ac28e0f893
Ethel C <cofresi.ethel@gmail.com>
2018-10-03 13:08:50 +0000

    automatically backed up by learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
-  include ActiveModel::Validations
+
     validates :name, presence: true
     validates :email, uniqueness: {:case_sensitive => false}
     validates :phone_number, :phone_number => {:ten_digits => true}
